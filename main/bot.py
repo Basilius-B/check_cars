1f520a6ed1420697cb43db0a488ec6cdca447092
Basilius <basilius.cooper@gmail.com>
2023-02-14 11:48:47 +0300

    added new python bot lib

--- main/bot.py
+++ main/bot.py
@@ -1,40 +1,29 @@
 import os
 import time
 import requests
-from aiogram.types import InputMediaPhoto
 from bs4 import BeautifulSoup
 import re
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
-from aiogram import Bot, Dispatcher, executor, types
+import telebot
 import asyncio
 from dotenv import load_dotenv
 
 project_folder = os.path.expanduser('~\check_cars')
-load_dotenv(os.path.join(project_folder, '.env'))
+load_dotenv('.env')
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+print(BOT_TOKEN)
 urls = [
     'https://cars.av.by/filter?brands[0][brand]=6&year[min]=2000&price_usd[min]=4000&price_usd[max]=12000&engine_capacity[min]=1900&engine_capacity[max]=2700&transmission_type=2&engine_type[0]=5&engine_type[1]=1&place_region[0]=1003&place_region[1]=1006&place_region[2]=1005&sort=4'
 ]
 # Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN)
+bot = telebot.TeleBot(token=BOT_TOKEN)
 
 
-async def is_enabled():
-    print('tut')
+def is_enabled():
+    print('start_loop')
     headers = {
         "User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    # chrome_options.add_argument("--remote-debugging-port=9222 https://chromium.org")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--start-maximized")
-    driver = webdriver.Edge()
-    await bot.send_message(chat_id='-695378182', text='start work')
+    bot.send_message(chat_id='-695378182', text='start work')
     while True:
         for url in urls:
             page = requests.get(url, headers=headers)
@@ -64,30 +53,14 @@ async def is_enabled():
                 msg_text = 'цена ' + price_usd + \
                            '\nгород -' + city + '\n' + \
                            '\n'.join(params) + \
-                           '\nhttps://cars.av.by' + soup.find('a', class_='listing-item__link').get('href')
+                           '\nhttps://cars.av.by' + link
                 try:
-                    print('https://cars.av.by' + link)
-                    driver.get('https://cars.av.by' + link)
-                    driver.find_element(By.CLASS_NAME, "gallery__fullscreen-button").click()
-                    list_link = driver.find_elements(By.CLASS_NAME, 'fullscreen-gallery__item')
-                    photos = [i.find_element(By.TAG_NAME, 'img').get_attribute('data-src') for i in list_link]
-                    photos_links = [InputMediaPhoto(photo, caption=msg_text) if i == 0 else InputMediaPhoto(photo) for
-                                    i, photo in enumerate(photos[::len(photos) // 10])]
-                    await bot.send_media_group(chat_id='-695378182', media=photos_links[:9])
-                    # await bot.send_message(chat_id='-695378182', text=msg_text)
-
+                    bot.send_message(chat_id='-695378182', text=msg_text)
                 except Exception as e:
                     print(e)
-                    e = str(e)
-                    if 'Flood control' in e:
-                        try:
-                            time.sleep(int(e.split(' ')[-2]) + 5)
-                            await bot.send_message(chat_id='-695378182', text=msg_text)
-                            await bot.send_media_group(chat_id='-695378182', media=photos_links[:9])
-                        except Exception as e2:
-                            print(e2)
+
         print('start_sleep')
-        time.sleep(60*2)
+        time.sleep(60 * 2)
 
 
 if __name__ == "__main__":
